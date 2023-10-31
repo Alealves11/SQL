ce1ea14100015143c67ecb817367f254666947c4
Alexandre Alves Pinheiro <96069682+Alealves11@users.noreply.github.com>
2023-10-31 11:08:46 -0300

    Update and rename teste.sql to SelectSimples.sql

---
+++ Básico/Primeiro código/SelectSimples.sql
@@ -0,0 +1,31 @@
+/*
+
+O SELECT simples utilizamos o caractere especial * para recuperar todas as linhas e colunas de uma tabela.
+Em boas práticas não devemos utilizá-lo, o ideal é colocar os nomes das colunas desejadas.
+
+
+Sintáxe:
+
+SELECT -> SELECIONE
+  *    -> [Todas as colunas]	
+FROM   -> [nome da tabela]
+		
+*/
+
+
+
+
+SELECT * FROM Employees;
+
+
+
+EmployeeID		LastName		FirstName		Title
+1				Davolio			Nancy			Sales Representative
+2				Fuller			Andrew			Vice President, Sales
+3				Leverling		Janet			Sales Representative
+4				Peacock			Margaret		Sales Representative
+5				Buchanan		Steven			Sales Manager
+6				Suyama			Michael			Sales Representative
+7				King			Robert			Sales Representative
+8				Callahan		Laura			Inside Sales Coordinator
+9				Dodsworth		Anne			Sales Representative
